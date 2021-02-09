7c92e7c5f3c696c12015e722de095903337652ec
Mahmoud Al-Haroon <mahmoud.harooney.tooling@gmail.com>
2021-02-09 19:53:20 +0200

    Size Configuration Edited

--- lib/size_config.dart
+++ lib/size_config.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+enum ScreenSize {
+  SMALL,
+  MEDIUM_1,
+  MEDIUM_2,
+  LARGE,
+}
+
 class SizeConfig {
   static MediaQueryData _mediaQueryData;
   static double screenWidth;
@@ -12,6 +19,54 @@ class SizeConfig {
     screenWidth = _mediaQueryData.size.width;
     screenHeight = _mediaQueryData.size.height;
     orientation = _mediaQueryData.orientation;
+  }
+}
+
+ScreenSize fetchScreenSizeInHeight(double height) {
+  if (height < 600) {
+    return ScreenSize.SMALL;
+  } else if (height < 700 && height > 600) {
+    return ScreenSize.MEDIUM_1;
+  } else if (height < 800 && height > 700) {
+    return ScreenSize.MEDIUM_2;
+  } else {
+    return ScreenSize.LARGE;
+  }
+}
+
+double setWidgetHeight(ScreenSize screenSize, BuildContext context,
+    double small, double medium1, double medium2, double large) {
+  switch (screenSize) {
+    case ScreenSize.SMALL:
+      return MediaQuery.of(context).size.height * small;
+      break;
+    case ScreenSize.MEDIUM_1:
+      return MediaQuery.of(context).size.height * medium1;
+      break;
+    case ScreenSize.MEDIUM_2:
+      return MediaQuery.of(context).size.height * medium2;
+      break;
+    case ScreenSize.LARGE:
+      return MediaQuery.of(context).size.height * large;
+      break;
+  }
+}
+
+double setWidgetWidth(ScreenSize screenSize, BuildContext context, double small,
+    double medium1, double medium2, double large) {
+  switch (screenSize) {
+    case ScreenSize.SMALL:
+      return MediaQuery.of(context).size.width * small;
+      break;
+    case ScreenSize.MEDIUM_1:
+      return MediaQuery.of(context).size.width * medium1;
+      break;
+    case ScreenSize.MEDIUM_2:
+      return MediaQuery.of(context).size.width * medium2;
+      break;
+    case ScreenSize.LARGE:
+      return MediaQuery.of(context).size.width * large;
+      break;
   }
 }
 
